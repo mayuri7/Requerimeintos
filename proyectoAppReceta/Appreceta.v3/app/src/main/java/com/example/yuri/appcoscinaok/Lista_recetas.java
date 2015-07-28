7b373b4abcbe7aad807a0cfbcec583b269c8670c
ccahuana <ccahuanahm@gmail.com>
2015-07-28 08:45:15 +0200

    lista de recetas
    insertando un nuevo layout lista_recetas

--- proyectoAppReceta/Appreceta.v3/app/src/main/java/com/example/yuri/appcoscinaok/Lista_recetas.java
+++ proyectoAppReceta/Appreceta.v3/app/src/main/java/com/example/yuri/appcoscinaok/Lista_recetas.java
@@ -1,31 +1,23 @@
 package com.example.yuri.appcoscinaok;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Lista_recetas extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.BtnRecetas).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Lista_recetas.class));
-            }
-        });
+        setContentView(R.layout.activity_lista_recetas);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_lista_recetas, menu);
         return true;
     }
 
