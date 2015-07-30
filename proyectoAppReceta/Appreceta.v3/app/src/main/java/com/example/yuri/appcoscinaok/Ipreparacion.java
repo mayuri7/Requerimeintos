e74ff60d9a7566651503137cb6087887d3e287df
ccahuana <ccahuanahm@gmail.com>
2015-07-30 20:54:12 +0200

    corrigiendo los lislas
    me deja error no dejaba de entrar a mi app

--- proyectoAppReceta/Appreceta.v3/app/src/main/java/com/example/yuri/appcoscinaok/Ipreparacion.java
+++ proyectoAppReceta/Appreceta.v3/app/src/main/java/com/example/yuri/appcoscinaok/Ipreparacion.java
@@ -1,42 +1,23 @@
 package com.example.yuri.appcoscinaok;
-import android.app.Activity;
-import android.content.Intent;
+
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-
-public class MainActivity extends Activity{
 
 
-    public MainActivity() {
-    }
+public class Ipreparacion extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, preparacion.class));
-            }
-        });
-        findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Ipreparacion.class));
-            }
-        });
-
-
+        setContentView(R.layout.activity_ipreparacion);
     }
 
-
-   @Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_ipreparacion, menu);
         return true;
     }
 
