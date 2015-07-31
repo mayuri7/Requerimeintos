d6305de2c77ddcfe1662e2fdbf99b2df4e268a8a
ccahuana <ccahuanahm@gmail.com>
2015-07-31 03:24:01 +0200

    lista de recetas
    insertando imagenes para la lista de receta

--- proyectoAppReceta/Appreceta.v3/app/src/main/java/com/example/yuri/appcoscinaok/MainActivity.java
+++ proyectoAppReceta/Appreceta.v3/app/src/main/java/com/example/yuri/appcoscinaok/MainActivity.java
@@ -36,6 +36,12 @@ public class MainActivity extends Activity{
                 startActivity(new Intent(MainActivity.this, configuracion.class));
             }
         });
+        findViewById(R.id.BtnRecetas).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, listarecetas.class));
+            }
+        });
 
 
     }
